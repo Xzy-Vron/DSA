ffc9f8bad771ff04863d72755f03b57fd31cfb0c
Avinash Ganore <avinashganore@gmail.com>
2025-12-27 10:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.60%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int right =nums.size() -1;
-        int left = 0;
-        vector<int> ans(nums.size(), 1);
+        unordered_map<int, int> count = {{0, 0}, {1, 0}, {2, 0}};
 
-        for (int i =0; i<nums.size(); i++) {
-            if (nums[i] == 0) {
-                ans[left] = nums[i];
-                left++;
-            }
-            else if (nums[i] == 2) {
-                ans[right] = nums[i];
-                right--;
-            }
+        for (int num : nums) {
+            count[num]++;
         }
 
-        for (int i =0; i<nums.size(); i++) {
-            nums[i]= ans[i];
-        }
+        int idx = 0;
+        for (int color = 0; color < 3; color++) {
+            int freq = count[color];
+            for (int j = 0; j < freq; j++) {
+                nums[idx] = color;
+                idx++;
+            }
+        }        
     }
 };
